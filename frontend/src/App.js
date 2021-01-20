ceaecdb5bbfe5aa620b1110284867318e951ab99
Dhruvam Zaveri <dhruvamzaveri14@gmail.com>
2021-01-20 09:11:46 +0530

    Added Navbar and Footer using react-bootstrap (npm library) and bootswatch (website)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 // import logo from './logo.svg';
 // import './App.css';
+import "./Components/Header.js";
+import Header from "./Components/Header.js";
+import Footer from "./Components/Footer.js";
+import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Proshop</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
