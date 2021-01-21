e05482f6393cc1ac846fce5648270246b1699568
Dhruvam Zaveri <dhruvamzaveri14@gmail.com>
2021-01-22 00:18:39 +0530

    Added routes to product page, cart, signup page and home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
-// import logo from './logo.svg';
-// import './App.css';
 import "./Components/Header.js";
 import Header from "./Components/Header.js";
 import Footer from "./Components/Footer.js";
 import HomeScreen from "./Screens/HomeScreen.js";
+import ProductScreen from "./Screens/ProductScreen.js";
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} exact />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
+
+/* The :id in product page route is placeholder for product._id, it will specify with product is to be rendered on the new page */
 
 export default App;
