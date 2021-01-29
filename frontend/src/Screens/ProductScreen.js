70c00002f0b35d4263a4bbd9bbfcb144d3c35834
dhruvam-zaveri <dhruvamzaveri14@gmail.com>
2021-01-29 09:20:46 +0530

    Added Product screen

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,7 +1,61 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, Card, ListGroup, Button } from "react-bootstrap";
+import Rating from "../Components/Rating.js";
+import products from "../products.js";
 
-const ProductScreen = () => {
-  return <div>Product</div>;
+const ProductScreen = ({ match }) => {
+  const product = products.find((p) => p._id === match.params.id);
+
+  return (
+    <>
+      <Link className="btn btn-lg" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid></Image>
+        </Col>
+        <Col md={6}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>Price: $ {product.price}</ListGroup.Item>
+            <ListGroup.Item>
+              <Row>
+                <Col>
+                  Status:{" "}
+                  <strong>
+                    {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                  </strong>
+                </Col>
+                <Col>Quantity:</Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Product Description: {product.description}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                className="btn btn-dark"
+                disabled={product.countInStock === 0}
+                block
+              >
+                Add to Cart
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default ProductScreen;
