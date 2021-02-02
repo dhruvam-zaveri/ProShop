fae74c6efd9c564bdbc3f990d017b273c575999b
dhruvam-zaveri <dhruvamzaveri14@gmail.com>
2021-02-03 02:02:28 +0530

    Added environment variables using dotenv (npm package). All variables are declared in .env file at root level, this file might not be pushed due to gitignore.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require("express"); //CommonJS expression; different from ES6 JS
 const products = require("./data/products.js");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express(); // initialize express
 
@@ -22,4 +25,9 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
