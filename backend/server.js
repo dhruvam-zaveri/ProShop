19503a068e620a88b9fc39084ae865c0731ce4fc
dhruvam-zaveri <dhruvamzaveri14@gmail.com>
2021-02-02 09:11:18 +0530

    Added backend folder and made a "start" script in package.json in root level which will run "node backend/server" command on "npm start".

---
+++ backend/server.js
@@ -0,0 +1,5 @@
+const express = require("express"); //CommonJS expression; different from ES6 JS
+
+const app = express(); // initialize express
+
+app.listen(5000, console.log("Server running on port 5000"));
