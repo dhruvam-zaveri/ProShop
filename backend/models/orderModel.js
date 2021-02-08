cbf6e0f0f53729d17693e24fc026f5e10c91359e
dhruvam-zaveri <dhruvamzaveri14@gmail.com>
2021-02-08 14:31:44 +0530

    Created order schema and model

---
+++ backend/models/orderModel.js
@@ -0,0 +1,94 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    // We can also define fields like:
+    // fieldName: typeOfField
+    // But we want to add extra properties besides of type, so we will be using object instead
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+
+    orderItem: [
+      {
+        name: { type: String, required: true },
+        qty: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: "Product",
+        },
+      },
+    ],
+    shippingAddress: {
+      address: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      postalCode: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    paymentResult: {
+      id: { type: String, required: true },
+      status: { type: String, required: true },
+      update_time: { type: String, required: true },
+      email_address: { type: String, required: true },
+    },
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    timeStamps: true,
+  }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+export const Order;
