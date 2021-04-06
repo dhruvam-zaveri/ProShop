156b90c8879373a3c2cbbd54fcf6dda161512bc0
dhruvam-zaveri <dhruvamzaveri14@gmail.com>
2021-04-07 00:13:00 +0530

    Edited Header.js to reflect user login in navbar. Also added logout option in navbar, added action for logout in userActions.js.

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -2,6 +2,7 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
+  USER_LOGOUT,
 } from "../constants/userConstants.js";
 
 import axios from "axios";
@@ -39,4 +40,11 @@ export const login = (email, password) => async (dispatch) => {
           : error.response,
     });
   }
+};
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("userInfo");
+  dispatch({
+    type: USER_LOGOUT,
+  });
 };
