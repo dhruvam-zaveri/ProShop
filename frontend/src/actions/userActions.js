428a6a14d8f235df910a2fd1654b53eef088da02
dhruvam-zaveri <dhruvamzaveri14@gmail.com>
2021-04-15 19:12:59 +0530

    Created orderDetails constants, reducers and action.

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -149,7 +149,7 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
       },
     };
 
-    const { data } = await axios.post(`/api/users/profile`, user, config);
+    const { data } = await axios.put(`/api/users/profile`, user, config);
 
     dispatch({
       type: USER_UPDATE_PROFILE_SUCCESS,
