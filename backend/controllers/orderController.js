bb255d2b774cc38b459e56395678ab89620683e7
dhruvam-zaveri <dhruvamzaveri14@gmail.com>
2021-04-21 10:55:22 +0530

    Created route and controller to get all the orders of a logged in user.

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -76,4 +76,14 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid };
+//@desc   Get orders of logged in user
+//@route  GET /api/orders/myorders
+//@access private
+const getMyOrders = asyncHandler(async (req, res) => {
+  console.log(req.user.id);
+  console.log("hi");
+  const orders = await Order.find({ user: req.user._id });
+  res.json(orders);
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
