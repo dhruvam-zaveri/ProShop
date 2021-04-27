5357a8ab1709dea68fce02cc6eec86b81963292a
dhruvam-zaveri <dhruvamzaveri14@gmail.com>
2021-04-27 23:21:41 +0530

    Deploying on heroku

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
@@ -15,11 +16,6 @@ connectDB();
 const app = express(); // initialize express
 
 // Creating routes
-app.get("/", (req, res) => {
-  // Route to main page
-  res.send("API is running!!");
-});
-
 // This will allow us to accept JSON data in body
 app.use(express.json());
 
@@ -32,6 +28,20 @@ app.use("/api/orders", orderRoutes);
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 // To deal with 404 errors; if any route other than specified is tried to access
 app.use(notFound);
