af510ff824e5aed436911e0532d87578eb2d0119
dhruvam-zaveri <dhruvamzaveri14@gmail.com>
2021-05-04 19:54:54 +0530

    Create uploadRoutes file to handle image routing in backed (used multer library of npm) and also updated server.js to configure upload routes and made the uploads folder in main directory static.

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import saleRoutes from "./routes/saleRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -23,12 +24,14 @@ app.use("/api/products", productRoutes);
 app.use("/api/sale", saleRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/uploads", uploadRoutes);
 
 // when we are ready we will hit this route and fetch the client ID
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
+// making uploads folder static
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
